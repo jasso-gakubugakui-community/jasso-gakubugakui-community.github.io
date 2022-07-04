e65f1769ab9ae14dc36ea13dbf27273dde66a5c1
Mikihisa Yuasa <myuasa2@illinois.edu>
2022-07-04 16:33:31 -0500

    feat: fonts applied

--- src/components/layout/theme.tsx
+++ src/components/layout/theme.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import '@fontsource/noto-sans-jp'
+import '@fontsource/roboto'
 
 import { CssBaseline } from "@material-ui/core";
 import { createTheme } from '@material-ui/core/styles';
@@ -13,7 +15,14 @@ export const MuiThemeProvider = (props: { children: React.ReactNode }) => {
             secondary: {
                 main: '#f8b500'
             }
-        }
+        },
+        typography: {
+            fontFamily: [
+                '"Noto Sans JP"',
+                'Roboto',
+                'sans-serif',
+            ].join(',')
+        },
     })
 
     return (
