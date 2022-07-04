f496e5082944292d96160a8d52e434faa9711da4
Mikihisa Yuasa <myuasa2@illinois.edu>
2022-07-04 17:43:04 -0500

    fix: hook is now inside

--- src/components/button/mediaButtons.tsx
+++ src/components/button/mediaButtons.tsx
@@ -4,38 +4,40 @@ import { Box, IconButton } from "@material-ui/core";
 import { Email, Twitter } from "@material-ui/icons";
 import { graphql, useStaticQuery } from "gatsby";
 
-const { site } = useStaticQuery<MediaQuery>(
-    graphql`
-        query Media {
-            site {
-                siteMetadata {
-                    twitterUsername
-                    email
-                }
-            }
-        }
-        `);
 
-export const EmailButton = () => {
-
-    const mail_link: string = `mailto:${site.siteMetadata.email}`
-    return (
-        <IconButton aria-label="email link" href={mail_link}>
-            <Email />
-        </IconButton>
-    )
-};
-
-export const TwitterButton = () => {
-    const twitter_link: string = `https://twitter.com/${site.siteMetadata.twitterUsername.slice(1)}`
-    return (
-        <IconButton aria-label="twitter link" href={twitter_link}>
-            <Twitter />
-        </IconButton>
-    )
-};
 
 export const MediaButtons = () => {
+
+    const { site } = useStaticQuery<MediaQuery>(
+        graphql`
+            query Media {
+                site {
+                    siteMetadata {
+                        twitterUsername
+                        email
+                    }
+                }
+            }
+            `);
+
+    const EmailButton = () => {
+
+        const mail_link: string = `mailto:${site.siteMetadata.email}`
+        return (
+            <IconButton aria-label="email link" href={mail_link}>
+                <Email />
+            </IconButton>
+        )
+    };
+
+    const TwitterButton = () => {
+        const twitter_link: string = `https://twitter.com/${site.siteMetadata.twitterUsername.slice(1)}`
+        return (
+            <IconButton aria-label="twitter link" href={twitter_link}>
+                <Twitter />
+            </IconButton>
+        )
+    };
     return (<Box><EmailButton /><TwitterButton /></Box>)
 }
 
