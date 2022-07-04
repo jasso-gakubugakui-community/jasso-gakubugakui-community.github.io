a585b941ef5e2065a64b9ae456f5ffb43cdd4ea0
Mikihisa Yuasa <myuasa2@illinois.edu>
2022-07-04 14:01:05 -0500

    fix: typo

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export const Layout = (childeren: React.ReactNode) => {
+export const Layout = (props: { children: React.ReactNode }) => {
 
     return (
         <div>
-            <main>{childeren}</main>
+            <main>{props.children}</main>
             <footer>Copyright © 2022 - {new Date().getFullYear()} JASSO学部学位取得型奨学生コミュニティ | All Rights Reserved</footer>
         </div>
     )
