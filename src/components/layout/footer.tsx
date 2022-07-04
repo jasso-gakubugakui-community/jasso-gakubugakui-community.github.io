5d7c432fea5467ad184de6589e4e2efc2b9a8c1e
Mikihisa Yuasa <myuasa2@illinois.edu>
2022-07-04 16:50:21 -0500

    fix: footer year display

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+import { Box, Container, Typography } from "@material-ui/core";
+
 export const Footer = () => {
+
+    const currentYear: number = new Date().getFullYear()
+    const websiteStartYear: number = 2022
+
+    const copyrightYears: string = currentYear == websiteStartYear ? websiteStartYear.toString() : `${websiteStartYear} - ${currentYear}`
+
     return (
-        <footer>
-            Copyright © 2022 - {new Date().getFullYear()} JASSO学部学位取得型奨学生コミュニティ | All Rights Reserved
-        </footer>
+        <Container maxWidth='xl'>
+            <Typography>
+                Copyright © {copyrightYears} JASSO学部学位取得型奨学生コミュニティ | All Rights Reserved
+            </Typography>
+        </Container>
     )
 }
