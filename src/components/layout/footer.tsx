7452916313f38b5447d1b81e41a526a44397e3eb
Mikihisa Yuasa <myuasa2@illinois.edu>
2022-07-04 15:33:26 -0500

    ref: header and fotter decoupled

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <footer>
+            Copyright © 2022 - {new Date().getFullYear()} JASSO学部学位取得型奨学生コミュニティ | All Rights Reserved
+        </footer>
+    )
+}
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import { Footer } from "./footer";
+
 export const Layout = (props: { children: React.ReactNode }) => {
 
     return (
         <div>
             <main>{props.children}</main>
-            <footer>Copyright © 2022 - {new Date().getFullYear()} JASSO学部学位取得型奨学生コミュニティ | All Rights Reserved</footer>
+            <Footer />
         </div>
     )
 }
