cdbc80804f35af099e914d38b4438b7492cd59b9
Mikihisa Yuasa <myuasa2@illinois.edu>
2022-07-06 21:23:38 -0500

    fix: mui import

--- src/components/layout/theme.tsx
+++ src/components/layout/theme.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import '@fontsource/noto-sans-jp'
 import '@fontsource/roboto'
 
-import { CssBaseline } from "@material-ui/core";
-import { createTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from "@material-ui/core/styles";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
 export const MuiThemeProvider = (props: { children: React.ReactNode }) => {
     const theme = createTheme({
