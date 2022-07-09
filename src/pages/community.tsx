6243fdb1dbd4d660f3197cebc0fd44e13894e907
Mikihisa Yuasa <myuasa2@illinois.edu>
2022-07-09 10:06:40 -0500

    feat: community guideline added

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -5,7 +5,7 @@ import { Box } from "@mui/system";
 
 import { Layout } from "../components/layout/layout"
 import { SEO } from "../components/seo";
-import { Paragraph, PageSectionHeading } from "../components/layout/paragraph";
+import { P, SectionHeading, SubsectionHeading } from "../components/layout/paragraph";
 import { PageNav } from "../components/navigation/pageNav";
 import { RegistrationButton } from "../components/button/mediaButtons";
 
@@ -20,8 +20,8 @@ const CommunityPage = () => {
             <Container maxWidth='lg' sx={{ marginBottom: 2 }}>
                 <PageNav pageName="Community" />
                 <Box>
-                    <PageSectionHeading>{site.siteMetadata.defaultTitle} Slack とは？</PageSectionHeading>
-                    <Paragraph>
+                    <SectionHeading>{site.siteMetadata.defaultTitle} Slack とは？</SectionHeading>
+                    <P>
                         <ul>
                             <li>
                                 <b>これから留学する派遣学生</b>
@@ -34,10 +34,10 @@ const CommunityPage = () => {
                             </li>
                         </ul>
                         をオンラインで繋ぐSlackコミュニティです。2022年8月に運営を開始しました。
-                    </Paragraph>
+                    </P>
                     <RegistrationButton />
-                    <PageSectionHeading>参加資格</PageSectionHeading>
-                    <Paragraph>
+                    <SectionHeading>参加資格</SectionHeading>
+                    <P>
                         当コミュニティはソーシャルメディア的な側面もあるため、参加者の保護、トラブル防止などの観点から、以下の5点を満たす方のみの登録を受け付けています。
                         <ol>
                             <li>現在JASSO海外留学支援制度学部学位取得型の派遣学生である、もしくは過去派遣学生であり、大学を卒業した。</li>
@@ -47,27 +47,63 @@ const CommunityPage = () => {
                             <li>その他、下記コミュニティガイドラインの内容に同意できる。</li>
                         </ol>
                         以上を全て満たし、JASSO海外学部学位取得型派遣学生 Slack Workplaceへの参加をご希望される方は以下のボタンからフォームへのご記入をお願いします。登録していただいたメールアドレスにコミュニティから招待メールを送らせていただきます。ご不明な点がありましたら、メールにて遠慮なくお問い合わせください。派遣学生の皆様のご参加をお待ちしております。
-                    </Paragraph>
+                    </P>
                     <RegistrationButton />
-                    <PageSectionHeading>Community Guideline</PageSectionHeading>
-                    <Paragraph>
-                        JASSO学部学位取得型派遣学生コミュニティは、2022年に有志のJASSO学部学位取得型支援制度の派遣学生達によってスタートした、現派遣学生及び卒業生の縦横の繋がりを作るためのコミュニティです。
-                    </Paragraph>
-                    <Paragraph>
-                        2022年当時、派遣学生同氏の強固な繋がりがある他奨学団体と比較して、JASSO学部学位取得型支援制度にはその様な繋がりがありませんでした。そこで我々は派遣学生主導のコミュニティを作りたいと考えました。
-                    </Paragraph>
-                    <Typography variant="h6" gutterBottom>
-                        1. 派遣学生情報発信サイト
-                    </Typography>
-                    <Paragraph>
-                        <b>現派遣学生及び卒業生に役立つ情報発信</b>を行います。
-                    </Paragraph>
-                    <Typography variant="h6" gutterBottom>
-                        2. 現派遣学生及び卒業生を繋ぐコミュニティ
-                    </Typography>
-                    <Paragraph>
-                        また2022年8月にはSlackワークスペースを立ち上げ、<b>オンラインコミュニティとしての役割</b>も果たすようになりました。
-                    </Paragraph>
+                    <SectionHeading>Community Guideline</SectionHeading>
+                    <SubsectionHeading>1. アンチハラスメントポリシー</SubsectionHeading>
+                    <P>
+                        特定の社会的属性に基づく差別的言動や各種ハラスメント、またJASSO学部学位コミュニティ運営がハラスメントと判断した行為を禁止します。行為が発覚した際は、運営メンバーから該当者へ警告をします。
+                    </P>
+                    <P>
+                        もしハラスメントを受けていると感じたら、気兼ねなく運営にお問い合わせフォームから相談してください。
+                    </P>
+                    <SubsectionHeading>2. 外部団体・企業による宣伝及び勧誘活動に関して</SubsectionHeading>
+                    <P>
+                        JASSO学部学位コミュニティでは学生が参加者の大多数を占めているという性質から、企業・団体による過剰な採用・企業宣伝の場になってしまう恐れがあります。
+                    </P>
+                    <P>
+                        外部企業・団体によるイベントの宣伝・勧誘・リクルーティング活動に関するトピックに関連する投稿は以下のルールに沿って行うこととします。
+                        <ol>
+                            <li>ダイレクトメッセージ(DM)の利用における注意
+                                <br />
+                                参加者が過剰なリクルーティング・宣伝をDMでされた場合、参加者はSlackの問い合わせフォームから運営へ通報することができます。
+                            </li>
+                        </ol>
+                    </P>
+                    <SubsectionHeading>3. DM・チャンネルの利用方法</SubsectionHeading>
+                    <P>
+                        コミュニティ内におけるダイレクトメッセージ（DM）・チャンネルの利用方法に関して以下のルールをご覧ください。
+                        <ol>
+                            <li>
+                                DMは、コミュニティガイドライン違反の通報がある場合、複数の管理者の合意のもと内容を閲覧することができます。完全なプライベート空間ではありません。
+                            </li>
+                            <li>
+                                新規チャンネルの作成を希望する際は、運営に問い合わせフォームからご連絡ください。運営メンバーの承認を得てから、新規チャンネルを作成するかどうかの判断をします。
+                            </li>
+                            <li>
+                                各投稿に関して議論を始めるもしくは返信をする際にはその投稿にスレッドを作成する形式を原則とします。ただ、自身の投稿をチャンネル参加者の目に留まるようにする場合、「also send to # チャンネル名」のオプションを使ってください。
+                            </li>
+                        </ol>
+                    </P>
+                    <SubsectionHeading>4. JASSO学部学位コミュニティへのコミットメント・退会手続きに関して</SubsectionHeading>
+                    <P>
+                        JASSO学部学位コミュニティのコミットメント・退会に関して以下のルールを遵守してください。
+                        <ol>
+                            <li>
+                                コミュニティ参加者が本コミュニティからの退会を希望する場合、運営への問い合わせフォームへご連絡ください。本Slackのアカウント停止手続きを行います。
+                            </li>
+                            <li>
+                                その他コミュニティガイドラインに反する行為が見受けられた場合（例: アカウントを本名で登録していない等）、運営から対象参加者に注意勧告をします。その連絡に対し返信が無い場合、運営は対象参加者のアカウント停止手続きを行います。
+                            </li>
+                        </ol>
+                    </P>
+                    <SubsectionHeading>5. ガイドラインの改訂に関して</SubsectionHeading>
+                    <P>
+                        JASSO学部学位コミュニティは、参加者の方のご意見が必要不可欠だと運営側は考えています。ガイドライン・プライバシーポリシーにおいては、今後コミュニティが拡大していくにつれ参加者の皆さんの提案や意見を元に適宜改訂していきます。
+                    </P>
+                    <P>
+                        ガイドライン及びプライバシーポリシーは「# general-コミュニティガイドライン」に固定投稿され、それに関する意見や議論もそのチャンネルで行うようにします。
+                    </P>
                 </Box>
             </Container>
 
