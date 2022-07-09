a138cca81e9e1c253813f52163cf0dfcdf03ec0e
Mikihisa Yuasa <myuasa2@illinois.edu>
2022-07-08 20:00:59 -0500

    feat: page content added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,19 +39,28 @@ const IndexPage = () => {
             {site.siteMetadata.defaultTitle}は、2022年に有志のJASSO海外留学支援制度学部学位取得型の派遣学生達によってスタートした、現派遣学生及び卒業生の縦横の繋がりを作るためのコミュニティです。
           </Paragraph>
           <Paragraph>
-            2022年当時、派遣学生同氏の強固な繋がりがある他奨学団体と比較して、JASSO海外留学支援制度学部学位取得型にはその様な繋がりがありませんでした。そこで我々は派遣学生主導のコミュニティを作りたいと考えました。
+            2022年当時、派遣学生同士の頻繁な交流がある他奨学団体と比較して、JASSO海外留学支援制度学部学位取得型にはその様な繋がりがありませんでした。そこで我々は派遣学生主導のコミュニティを作りたいと考えました。
           </Paragraph>
-          <Typography variant="h6" gutterBottom>
+          <Typography variant="h6" gutterBottom color='primary'>
             1. 派遣学生情報発信サイト
           </Typography>
           <Paragraph>
-            <b>現派遣学生及び卒業生に役立つ情報発信</b>を行います。
+            <b>留学前・中・後の派遣学生に役立つ情報発信</b>を行います。
           </Paragraph>
-          <Typography variant="h6" gutterBottom>
+          <Paragraph>
+            現在企画している発信内容として
+            <ul>
+              <li>留学前：派遣学生同士の情報提供・サポート</li>
+              <li>留学中：地域ごとのコミュニティ形成・他の地域の派遣学生との交流</li>
+              <li>留学後：現役派遣学生への留学支援、キャリア・進路情報共有及び相談</li>
+            </ul>
+            が考えられています。
+          </Paragraph>
+          <Typography variant="h6" gutterBottom color='primary'>
             2. 現派遣学生及び卒業生を繋ぐコミュニティ
           </Typography>
           <Paragraph>
-            また2022年8月にはSlackワークスペースを立ち上げ、<b>オンラインコミュニティとしての役割</b>も果たすようになりました。
+            2022年8月にはSlackワークスペースを立ち上げ、<b>オンラインコミュニティとしての役割</b>も始動しました。
           </Paragraph>
         </Box>
         <Toolbar disableGutters>
